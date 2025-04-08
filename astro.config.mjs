0be658ebce8904bf2cbad205f9fd19277ff5a6ea
Kabeer Jaffri <kabeer11000@gmail.com>
2025-04-09 04:20:36 +0500

    building with intl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,34 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
+import robotsTxt from "astro-robots-txt";
+import pageInsight from "astro-page-insight";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  i18n: {
+    // Map language names to standard locale codes
+    locales: ["en", "de", "ur"],
+    // Assuming English is the default language
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [react(), robotsTxt(), pageInsight(), sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        de: 'de-DE',
+        ur: 'ur-PK',
+      },
+    },
+  }), partytown()],
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,80 @@
+// locales: ["en", "de", "hi", "ur"],
+
+export const languages = {
+    en: 'English',
+    de: 'German',
+    ur: 'Urdu',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.software-and-design': 'Software + Design',
+        'nav.about': 'About',
+        'nav.work': 'Work',
+        'nav.experties': 'Experties',
+        'nav.lets-meet': 'Lets Meet',
+        'nav.menu': 'Menu',
+        'nav.close': 'Close',
+
+        'about.about-the-company-text': 'About The Company',
+        'about.about-otherdev': 'Driven by a commitment to innovation and inclusivity, our web development approach centers on creating digital experiences that are as accessible as they are engaging. Every project is guided by the belief that good design should empower, connect, and include all users, regardless of their background or abilities. By blending creativity with cutting-edge technology, we aim to build websites that not only meet today’s standards but also adapt and thrive in tomorrow’s digital landscape.\n\nOur team is passionate about crafting solutions that reflect these values—ensuring that every detail serves a purpose, enhances usability, and leaves a meaningful impact.',
+        'about.projects': 'Projects',
+
+        'footer.subscribe-to-our-newsletter': 'Subscribe to our newsletter',
+        'footer.mailing-address': 'Mailing Address',
+        'footer.easter-egg': 'Powered by Imagination.',
+        'footer.bottom-big-text': 'Lets Collaborate',
+        'footer.bottom-all-rights-reserved': 'All rights reserved.',
+
+
+        'skip-to-content': 'Skip to Content',
+        'work.view-website': 'View Website',
+    },
+    de: {
+        'nav.software-and-design': 'Software + Design',
+        'nav.about': 'Über uns',
+        'nav.work': 'Arbeit',
+        'nav.experties': 'Kompetenzen',
+        'nav.lets-meet': 'Lass uns treffen',
+        'nav.menu': 'Menü',
+        'nav.close': 'Schließen',
+
+        'about.about-the-company-text': 'Über das Unternehmen',
+        'about.about-otherdev': 'Angetrieben von einem Engagement für Innovation und Inklusivität konzentriert sich unser Webentwicklungsansatz auf die Schaffung digitaler Erlebnisse, die ebenso zugänglich wie ansprechend sind. Jedes Projekt wird von der Überzeugung geleitet, dass gutes Design alle Nutzer befähigen, verbinden und einbeziehen sollte, unabhängig von ihrem Hintergrund oder ihren Fähigkeiten. Durch die Verbindung von Kreativität mit modernster Technologie zielen wir darauf ab, Websites zu erstellen, die nicht nur die heutigen Standards erfüllen, sondern sich auch an die digitale Landschaft von morgen anpassen und darin erfolgreich sein können.\n\nUnser Team ist leidenschaftlich daran interessiert, Lösungen zu entwickeln, die diese Werte widerspiegeln – um sicherzustellen, dass jedes Detail einem Zweck dient, die Benutzerfreundlichkeit verbessert und einen bedeutsamen Einfluss hinterlässt.',
+        'about.projects': 'Projekte',
+
+        'footer.subscribe-to-our-newsletter': 'Abonnieren Sie unseren Newsletter',
+        'footer.mailing-address': 'Postanschrift',
+        'footer.easter-egg': 'Angetrieben von Vorstellungskraft.',
+        'footer.bottom-big-text': 'Lass uns zusammenarbeiten',
+        'footer.bottom-all-rights-reserved': 'Alle Rechte vorbehalten.',
+
+        'skip-to-content': 'Zum Inhalt springen',
+        'work.view-website': 'Website ansehen',
+    },
+    ur: {
+        'nav.software-and-design': 'سافٹ ویئر + ڈیزائن',
+        'nav.about': 'ہمارے بارے میں',
+        'nav.work': 'کام',
+        'nav.experties': 'مہارت',
+        'nav.lets-meet': 'آئیے ملتے ہیں',
+        'nav.menu': 'مینيو',
+        'nav.close': 'بند کریں',
+
+        'about.about-the-company-text': 'کمپنی کے بارے میں',
+        'about.about-otherdev': 'جدت اور شمولیت کے عزم کے تحت، ہمارا ویب ڈیولپمنٹ کا نقطہ نظر ایسے ڈیجیٹل تجربات تخلیق کرنے پر مرکوز ہے جو اتنے ہی قابل رسائی ہوں جتنے کہ دل چسپ۔ ہر پروجیکٹ اس یقین دہانی کے ساتھ رہنمائی کرتا ہے کہ اچھا ڈیزائن تمام صارفین کو بااختیار بنائے، جوڑے اور شامل کرے، قطع نظر ان کے پس منظر یا صلاحیتوں کے۔ جدید ترین ٹیکنالوجی کے ساتھ تخلیقی صلاحیتوں کو ملا کر، ہمارا مقصد ایسی ویب سائٹس بنانا ہے جو نہ صرف آج کے معیارات پر پورا اتریں بلکہ کل کے ڈیجیٹل منظر نامے میں ڈھل سکیں اور ترقی کر سکیں۔\n\nہماری ٹیم ایسے حل تیار کرنے کے لیے پرجوش ہے جو ان اقدار کی عکاسی کرتے ہیں — اس بات کو یقینی بناتے ہوئے کہ ہر تفصیل ایک مقصد پورا کرتی ہے، استعمال میں آسانی کو بڑھاتی ہے، اور ایک بامعنی اثر چھوڑتی ہے۔',
+        'about.projects': 'پراجیکٹس',
+
+        'footer.subscribe-to-our-newsletter': 'ہمارے نیوز لیٹر کو سبسکرائب کریں',
+        'footer.mailing-address': 'ڈاک کا پتہ',
+        'footer.easter-egg': 'تخیل کی طاقت سے چلتا ہے۔',
+        'footer.bottom-big-text': 'آئیے تعاون کرتے ہیں',
+        'footer.bottom-all-rights-reserved': 'جملہ حقوق محفوظ ہیں۔',
+
+        'skip-to-content': 'مواد پر جائیں',
+        'work.view-website': 'ویب سائٹ دیکھیں',
+
+    },
+} as const;
