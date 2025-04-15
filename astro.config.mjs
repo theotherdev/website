4a4467a0f6eca8d8e4e0d997a7f7b3fff4db8109
Kabeer Jaffri <kabeer11000@gmail.com>
2025-04-15 21:07:10 +0500

    should build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://www.otherdev.com",
+  compressHTML: true,
   url: "https://www.otherdev.com", // Optional redundancy for certain plugins
   vite: {
     plugins: [tailwindcss()],
